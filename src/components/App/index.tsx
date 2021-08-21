c4dd98b1437bddfaff8e44c2ffeef120716e2143
Jindřich Máca <tunik93@gmail.com>
2021-08-21 14:10:45 +0200

    🎨 Update project folder structure and clear project template code

---
+++ src/components/App/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import logo from './logo.svg'
+import './index.css'
+
+export const App: React.FC = () => (
+	<header className="App">
+		<h1>WeddinG</h1>
+		<img src={logo} className="App-logo" alt="logo" />
+	</header>
+)
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import { App } from './components/App'
+import reportWebVitals from './utils/reportWebVitals'
 import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
 	<React.StrictMode>
