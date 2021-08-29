74f2e04b2ec073d436b13303acffdc97b4a08f6f
Jana Chaloupková <janachaloupkova97@gmail.com>
2021-08-29 14:05:28 +0200

    💄 Add flavor to Sass and restructure structure

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,9 +3,51 @@ import React from 'react'
 import logo from './logo.svg'
 import './index.scss'
 
-export const App: React.FC = () => (
-	<header className="App">
-		<h1>WeddinG</h1>
-		<img src={logo} className="App__logo" alt="logo" />
-	</header>
-)
+export const App: React.FC = () => {
+	const pageTitle = 'WeddinG'
+
+	return (
+		<>
+			<header>
+				<h1>{pageTitle}</h1>
+				<img src={logo} className="header__logo" alt="logo" />
+			</header>
+			<nav>
+				{/* TODO */}
+				<ul>
+					<li>
+						<a href="#">WEDDING DATE</a>
+					</li>
+					<li>
+						<a href="#">GUEST FORM</a>
+					</li>
+					<li>
+						<a href="#">GUEST LIST</a>
+					</li>
+					<li>
+						<a href="#">SONG LIST</a>
+					</li>
+					<li>
+						<a href="#">ADMIN PAGE</a>
+					</li>
+					<li>
+						<a href="#">BUDGET FEATURE</a>
+					</li>
+					<li>
+						<a href="#">FIREBASE</a>
+					</li>
+				</ul>
+				{/* DONE */}
+				<ul>
+					<li>BASIC SET-UP AND DESIGN LOL</li>
+				</ul>
+			</nav>
+			<section className="content">
+				<p>~ PAGE CONTENT ~</p>
+			</section>
+			<footer>
+				<h1>{pageTitle}</h1>
+			</footer>
+		</>
+	)
+}
