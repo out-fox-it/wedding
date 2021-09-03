bc048f786d808778ae00ce7a86a3bbe64b4fa95e
Jindřich Máca <tunik93@gmail.com>
2021-09-03 15:48:24 +0200

    ✨ Implement stars on form input focus animation

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -76,20 +76,25 @@ export const App: React.FC = () => {
 				</p>
 
 				{/* INPUT DESIGN TEMPLATE */}
-				{/* Empty <span> is a hack for adding pseudoelements to input! */}
 				<form>
-					<input type="text" placeholder="NAME:" />
-					<span></span>
-					<input type="text" placeholder="PASSWORD:" />
-					<span></span>
-					<input type="text" placeholder="SOMETHING:" />
-					<span></span>
-					<textarea placeholder="LONG ANSWER:" />
-					<span></span>
-					<input type="text" placeholder="SHORT ANSWER:" />
-					<span></span>
-					<input className="button" type="button" value="SUBMIT" />
-					<span></span>
+					<label>
+						<input type="text" placeholder="NAME:" />
+					</label>
+					<label>
+						<input type="text" placeholder="PASSWORD:" />
+					</label>
+					<label>
+						<input type="text" placeholder="SOMETHING:" />
+					</label>
+					<label>
+						<textarea placeholder="LONG ANSWER:" />
+					</label>
+					<label>
+						<input type="text" placeholder="SHORT ANSWER:" />
+					</label>
+					<label>
+						<input className="button" type="button" value="SUBMIT" />
+					</label>
 				</form>
 
 				<p>
