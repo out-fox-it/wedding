8a78b85a6ca360fecd332dd6747945051ae822d8
Jindřich Máca <tunik93@gmail.com>
2022-08-10 23:43:16 +0200

    ✨ Add script for processing weather data

--- scripts/file-utils.ts
+++ scripts/file-utils.ts
@@ -6,3 +6,13 @@ export async function saveDataToJsonFile<T>(
 ): Promise<void> {
   await fs.writeFile(path, JSON.stringify(data, null, '\t'), 'utf8')
 }
+
+export async function readDataFromJsonFile(path: string): Promise<unknown> {
+  return JSON.parse(await fs.readFile(path, 'utf8')) as unknown
+}
+
+export async function listFilesInDirectory(
+  directoryPath: string,
+): Promise<string[]> {
+  return await fs.readdir(directoryPath)
+}
