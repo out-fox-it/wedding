b7bf98dbf988db32c94138d9e3d759f2d249f70d
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-08-25 22:18:36 +0200

    💄 Update LocaleSwitcher style

--- src/components/LocalesSwitcher/styled.ts
+++ src/components/LocalesSwitcher/styled.ts
@@ -1,8 +1,26 @@
 import styled from 'styled-components'
+import { colors } from '~/theme/colors'
+
+interface LiProps {
+  isCurrentLanguage: boolean
+}
 
 export const LocalesList = styled.ul`
-  list-style: none;
-  padding: 0;
   display: flex;
   gap: 1rem;
+  margin: 0;
+  justify-content: end;
+`
+
+export const StyledLi = styled.li<LiProps>`
+  border: 0.15rem solid
+    ${(props) => (props.isCurrentLanguage ? colors.text.base : 'transparent')};
+  border-radius: 15%;
+  padding: 0.2rem 0.6rem;
+  ${(props) => !props.isCurrentLanguage && 'filter: grayscale(70%);'};
+
+  & > a {
+    text-decoration: none;
+    font-size: 2.5rem;
+  }
 `
