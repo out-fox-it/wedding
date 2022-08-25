e7ec3e13d52a5c1fa2501aa1eb827cf8490908a5
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-08-25 22:17:41 +0200

    💄 Add theme

---
+++ src/theme/palette.ts
@@ -0,0 +1,12 @@
+export const palette = {
+  // Generic colors
+  white: 'hsl(0, 0%, 100%)',
+  gray: 'hsl(0, 0%, 50%)',
+  black: 'hsl(0, 0%, 0%)',
+
+  // Accent colors
+  pink: 'hsl(340, 82%, 59%)',
+  purple: 'hsl(264, 60%, 41%)',
+  purpleDarker: 'hsl(246, 23%, 16%)',
+  yellow: 'hsl(46, 99%, 49%)',
+}
