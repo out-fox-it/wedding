54278850ee58ac76af4f900a0671a8b5050e19b0
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-08-25 22:19:12 +0200

    ✨ Add Footer component

---
+++ src/components/Footer/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import { typography, htmlTagLookalike } from '~/theme/typography'
+
+export const StyledHtmlTagLookalike = styled.span`
+  ${typography.label.large}
+  ${htmlTagLookalike}
+`
