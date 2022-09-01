9365b3a73cb7ea6838993777acb785abfa1f848c
Jindřich Máca <tunik93@gmail.com>
2022-09-01 15:31:40 +0200

    ✨ Finish the address copying feature

--- src/features/HeaderFieldset/components/AddressFieldset.tsx
+++ src/features/HeaderFieldset/components/AddressFieldset.tsx
@@ -1,6 +1,7 @@
 import { useTranslations } from 'next-intl'
 import Link from 'next/link'
 import type { FC } from 'react'
+import { useRef } from 'react'
 
 import { CopyToClipboard } from '~/components/CopyToClipboard'
 import { Fieldset } from '~/components/Fieldset'
@@ -10,38 +11,17 @@ import { StyledLink } from '~/theme/typography'
 import { LinksContainer, StyledAddressContainer } from '../styled'
 
 export const AddressFieldset: FC = () => {
-  // TODO: Our solution falls apart when we want to access multiple objects through the useTranslation hook. It looks overcomplicated and is very hard to read.
-
-  // I would prefer solution that's used without itnl-next libray:
-  // https://react.i18next.com/latest/usetranslation-hook
-
-  // const { t } = useTranslation({ key1, key2 })
-  // t('title', { ns: 'key2' })
-
-  // However, I couldn't make this work over the documented error where
-  // TypeScript will lose the literal value, and it will infer the key as string (!):
-  // https://react.i18next.com/latest/typescript#type-errors
-
-  // Or maybe we just set up nesting in our JSON files wrong?
-  // Docs: https://www.i18next.com/translation-function/nesting
-
   const t = useTranslations('HeaderFieldset.AddressFieldset')
-  const tAddressLinks = 'headerFieldsetLinks'
-
-  // TODO: We should be able to get full address from children of CopyToClipboard component.
-  // Or at least get it from useTranslation hook BUT HOW CAN WE SPREAD THE DAMN OBJECT?!
-  // WHY DOESN'T {...tAddress} OR EVEN JSON.STRINGIFY() WORK :( :( :(
-
-  const fullAddress = 'Address :/'
+  const fullAddressRef = useRef<HTMLElement>(null)
 
   return (
     <Fieldset borderColor={colors.accent.purple} legendText={t('legendText')}>
       {/* TODO: Only show address AFTER log in. :) */}
       <CopyToClipboard
         title="Both addresses are valid and lead to the same house! :) No. 794 is a bit easier to spot."
-        copyRef={fullAddress}
+        copyRef={fullAddressRef}
       >
-        <StyledAddressContainer>
+        <StyledAddressContainer ref={fullAddressRef}>
           <span
             className="address-animation"
             data-address-1={`${t('street')} ${t('streetNumber.0')}`}
@@ -55,13 +35,13 @@ export const AddressFieldset: FC = () => {
       {/* TODO: Add links content! */}
       <LinksContainer>
         <Link href="/" passHref>
-          <StyledLink>{t(`${tAddressLinks}.map`)}</StyledLink>
+          <StyledLink>{t('headerFieldsetLinks.map')}</StyledLink>
         </Link>
         <Link href="/" passHref>
-          <StyledLink>{t(`${tAddressLinks}.parking`)}</StyledLink>
+          <StyledLink>{t('headerFieldsetLinks.parking')}</StyledLink>
         </Link>
         <Link href="/" passHref>
-          <StyledLink>{t(`${tAddressLinks}.hotels`)}</StyledLink>
+          <StyledLink>{t('headerFieldsetLinks.hotels')}</StyledLink>
         </Link>
       </LinksContainer>
     </Fieldset>
