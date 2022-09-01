ee6dfa01eb78b86e90b322673a20ac74c9fa24fd
Jindřich Máca <tunik93@gmail.com>
2022-09-01 15:31:40 +0200

    ✨ Finish the address copying feature

--- src/components/CopyToClipboard/index.tsx
+++ src/components/CopyToClipboard/index.tsx
@@ -1,17 +1,63 @@
-import type { ButtonHTMLAttributes, FC } from 'react'
+import type { ButtonHTMLAttributes, FC, RefObject } from 'react'
 
 import { Button } from '../Button'
 
 interface ICopyToClipboardProps
   extends ButtonHTMLAttributes<HTMLButtonElement> {
-  copyRef?: string
+  copyRef: RefObject<HTMLElement>
 }
+
+const isNodeText = (node: Node): node is Text =>
+  node.nodeType === Node.TEXT_NODE
+
+const isNodeHTMLElement = (node: Node): node is HTMLElement =>
+  node.nodeType === Node.ELEMENT_NODE && node instanceof HTMLElement
+
+const getPseudoElementContent = (
+  element: HTMLElement,
+  pseudoElement: '::before' | '::after',
+): string | null => {
+  const pseudoElementContent = getComputedStyle(element, pseudoElement).content
+
+  return pseudoElementContent !== 'none'
+    ? pseudoElementContent.replaceAll('"', '')
+    : null
+}
+
+const getElementCompleteText = (element: HTMLElement): string =>
+  Array.from(element.childNodes)
+    .reduce<string[]>((previousTexts, childNode) => {
+      if (isNodeText(childNode)) {
+        const text = childNode.wholeText
+        return [...previousTexts, ...(text !== '' ? [text] : [])]
+      }
+
+      if (isNodeHTMLElement(childNode)) {
+        const before = getPseudoElementContent(childNode, '::before')
+        const text = getElementCompleteText(childNode)
+        const after = getPseudoElementContent(childNode, '::after')
+
+        return [
+          ...previousTexts,
+          ...(before ? [before] : []),
+          ...(text !== '' ? [text] : []),
+          ...(after ? [after] : []),
+        ]
+      }
+
+      return previousTexts
+    }, [])
+    .join(' ')
 
 export const CopyToClipboard: FC<ICopyToClipboardProps> = (props) => {
   const { copyRef, title, children } = props
 
-  const copyToClipBoard = () =>
-    copyRef && void navigator.clipboard.writeText(copyRef)
+  const copyToClipBoard = (): void => {
+    const element = copyRef.current
+    if (!element) return
+
+    void navigator.clipboard.writeText(getElementCompleteText(element))
+  }
 
   return (
     <Button isClear title={title} onClick={copyToClipBoard}>
