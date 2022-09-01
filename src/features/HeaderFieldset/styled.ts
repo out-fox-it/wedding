d6156c740871363eb7bd3b3a058755e196b4ca1a
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-09-01 17:11:10 +0200

    🚚 Move and rename HeaderFieldset components

--- src/features/HeaderFieldset/styled.ts
+++ src/features/HeaderFieldset/styled.ts
@@ -40,29 +40,3 @@ export const FieldsetsContainer = styled.section`
     }
   }
 `
-
-export const StyledAddressContainer = styled.article`
-  text-transform: uppercase;
-  font-style: italic;
-
-  & > span:first-child::before {
-    content: attr(data-address-1);
-    animation: change-address infinite 7s;
-  }
-
-  @keyframes change-address {
-    0% {
-      content: attr(data-address-1);
-    }
-
-    50% {
-      content: attr(data-address-2);
-    }
-  }
-`
-
-export const LinksContainer = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-`
