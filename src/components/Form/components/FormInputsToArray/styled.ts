e34f624df48374a14fe147f7c0425eaa6db5a663
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-09-05 18:23:51 +0200

    ✨ Add FormInputsToArray feature to WeddingForm

---
+++ src/components/Form/components/FormInputsToArray/styled.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+import { colors } from '~/theme/colors'
+import { mq } from '~/theme/mq'
+import { typography } from '~/theme/typography'
+
+export const StyledFormInputsToArrayLabel = styled.label`
+  &:focus-within::before,
+  &:focus-within::after {
+    content: '';
+  }
+
+  & * div {
+    width: 100%;
+    position: relative;
+
+    & > span {
+      ${typography.label.small}
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      display: flex;
+      align-items: center;
+      opacity: 0.2;
+
+      ${mq.smallOnly} {
+        display: none;
+      }
+    }
+
+    & > input {
+      ${mq.medium} {
+        padding-left: 3rem;
+      }
+
+      ${mq.large} {
+        padding-left: 4rem;
+      }
+
+      width: 100%;
+      border: none;
+      border-bottom: 0.4rem double rgba(255, 255, 255, 0.2);
+    }
+  }
+
+  & * button {
+    padding-top: 3rem;
+    font-weight: bold;
+    color: ${colors.accent.purple};
+  }
+`
