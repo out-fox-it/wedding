ac0374683c157a25c5fc711c8440aa60217d4509
Jindřich Máca <tunik93@gmail.com>
2022-09-14 01:02:36 +0200

    🏷️ Setup types generation from GraphQL schema

---
+++ codegen.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/** @type {import("@graphql-codegen/cli").CodegenConfig} */
+module.exports = {
+  schema: './src/pages/api/schema/*.ts',
+  generates: {
+    './src/pages/api/resolvers/resolvers.generated.d.ts': {
+      plugins: [
+        {
+          add: {
+            content: '/* eslint-disable @typescript-eslint/no-explicit-any */',
+          },
+        },
+        'typescript',
+        'typescript-resolvers',
+      ],
+      config: {
+        useIndexSignature: true,
+      },
+    },
+  },
+  hooks: { afterAllFileWrite: ['prettier --write', 'eslint --fix'] },
+}
