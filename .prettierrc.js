5e5acb67760b1cd319b924293c7c6814e788c877
Jindřich Máca <tunik93@gmail.com>
2023-08-29 17:53:09 +0200

    💄 Install & configure daisyUI and other Tailwind utility libraries

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,5 @@
 module.exports = {
   ...require('@strv/prettier-config'),
   trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss'],
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,27 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          primary: '#ec407a',
+          secondary: '#5c2aa7',
+          accent: '#f9c001',
+          neutral: '#2a323c',
+          'base-100': '#222033',
+          info: '#3abff8',
+          success: '#36d399',
+          warning: '#fdba74',
+          error: '#f87272',
+        },
+      },
+    ],
+    logs: false,
+  },
 }
